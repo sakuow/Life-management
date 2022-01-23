7adc6ccb519549f8d0b1d70c109c5967a1762045
sakuow <yusaku1335@gmail.com>
2022-01-23 11:18:48 +0900

    user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: session_params[:name])#取得したユーザー名でユーザーを検索
+
+      if user&.authenticate(session_params[:password])#ユーザーが見つかった場合には、送られてきたパスワードによる認証をauthenticateメソッドで行う
+      session[:user_id]=user.id#認証に成功した場合に、セッションにuser_idを格納しています。
+
+      redirect_to root_path, notice: 'ログインしました'
+
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :schedules
   resources :spends
   resources :incomes
