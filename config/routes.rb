7ca86862c2187b45b678f717e8cc669ed713f987
sakuow <yusaku1335@gmail.com>
2022-01-26 11:52:31 +0900

    routing修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'sessions/new'
-  get 'sessions/create'
   root :to => 'homes#top'
   resources :schedules
-  resources :spends
-  resources :incomes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
