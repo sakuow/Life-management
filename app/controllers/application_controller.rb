64acb628ed9993a40ea9ac6781a414669d63b9ea
sakuow <yusaku1335@gmail.com>
2022-01-26 11:36:28 +0900

    header作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top]
+
 
   def after_sign_in_path_for(resource)
     schedules_path
   end
 
   def after_sign_out_path_for(resource)
-    root_path # ログアウト後に遷移するpathを設定
+    root_path
   end
 
   protected
